a35ad67769ad41671327a9dc0b0cec9d106e9eff
remon <remonshokry209@gmail.com>
2022-04-15 00:17:03 +0200

    Add IGeneral Repo

---
+++ MVCmasr/Repository/IGeneral.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MVCmasr.Repository
+{
+    public interface IGeneral<T>
+    {
+        public List<T> GetAll(); 
+        public T GetById(int _id); 
+        public int Insert(T _obj); 
+        public int Update(int _id , T _obj); 
+        public int Delete(int _id);
+    }
+}
